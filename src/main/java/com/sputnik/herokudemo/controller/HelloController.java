c95ae811c4bd1544b4982b9d8a7d0965da3028de
= <=>
2024-03-23 19:46:01 +0100

    CI/CD DEMO

--- src/main/java/com/sputnik/herokudemo/controller/HelloController.java
+++ src/main/java/com/sputnik/herokudemo/controller/HelloController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
     @GetMapping("/")
     public String hello(){
+
+        System.out.println("Called the endpoint");
 
         return "Hello Ujunwa";
     }
 }
+
